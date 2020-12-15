e4344fae7a8d8fd5ca3104bd163caef21597b806
adelekap <adelejkap@gmail.com>
2020-12-15 15:43:20 -0700

    solve day 7 pt 2

--- day7/determine_bag_contents.py
+++ day7/determine_bag_contents.py
@@ -1,12 +1,21 @@
 from day7.input import read_bag_input
 
+RULES = read_bag_input()
 
-def define_bags_with_shiny_gold() -> list:
-    rules = read_bag_input()
-    bags = [b for b in rules.bags.values() if b.number_of_bags_inside('shiny gold')]
+
+def find_bags_with_shiny_gold() -> list:
+    bags = [b for b in RULES.bags.values() if b.number_of_bags_inside('shiny gold')]
 
     return bags
 
 
+def find_num_bags_inside_shiny_gold() -> int:
+    bags = RULES.bags['shiny gold'].contents_count
+
+    return sum(bags.values())
+
+
 if __name__ == '__main__':
-    print(len(define_bags_with_shiny_gold()))
+    print(len(find_bags_with_shiny_gold()))
+
+    print(find_num_bags_inside_shiny_gold())
