6bc5e05d47d3aacad75e5bc5eefd9fc396208128
adelekap <adelejkap@gmail.com>
2020-12-16 11:17:31 -0700

    solve day 9 solve day 9

--- day9/find_flaw.py
+++ day9/find_flaw.py
@@ -4,7 +4,24 @@ from utils.exceptions import InfeasibleSolutionException
 PREAMBLE_SIZE = 25
 
 
-def find_sum(target_sum, options: list) -> tuple:
+def find_contiguous_sum_n(target_sum: int) -> list:
+    options = read_xmas_input()
+
+    for n in range(len(options)):
+        working_sum = options[n]
+        i = n + 1
+
+        while working_sum < target_sum and i < len(options):
+            working_sum += options[i]
+            i += 1
+
+        if working_sum == target_sum:
+            return options[n:i]
+
+    raise InfeasibleSolutionException
+
+
+def find_sum_in_window(target_sum: int, options: list) -> tuple:
     if len(options) >= 2:
         sum = options[0] + options[-1]
 
@@ -12,13 +29,13 @@ def find_sum(target_sum, options: list) -> tuple:
             return options[0], options[-1]
 
         if sum > target_sum:
-            return find_sum(target_sum, options[:-1])
+            return find_sum_in_window(target_sum, options[:-1])
 
-        return find_sum(target_sum, options[1:])
+        return find_sum_in_window(target_sum, options[1:])
 
 
 def sum_possible(sum: int, options: list) -> bool:
-    result = find_sum(sum, sorted([o for o in options if o <= sum]))
+    result = find_sum_in_window(sum, sorted([o for o in options if o <= sum]))
 
     return result is not None
 
@@ -36,5 +53,15 @@ def find_xmas_flaw() -> int:
     raise InfeasibleSolutionException
 
 
+def find_encryption_weakness() -> int:
+    xmas_flaw = find_xmas_flaw()
+    contiguous_sum_n = find_contiguous_sum_n(xmas_flaw)
+    sorted_n = sorted(contiguous_sum_n)
+
+    return sorted_n[0] + sorted_n[-1]
+
+
 if __name__ == '__main__':
     print(find_xmas_flaw())
+
+    print(find_encryption_weakness())
