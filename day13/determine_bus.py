2fefd974f9f570f28ae4113a439ffe3daa726692
adelekap <adelejkap@gmail.com>
2020-12-20 10:59:06 -0700

    solve day 13 part 2

--- day13/determine_bus.py
+++ day13/determine_bus.py
@@ -1,11 +1,10 @@
-from math import ceil
 import numpy as np
 
 from day13.input import read_bus_input
 
 
-def heuristic_start(bus_ids: list) -> int:
-    return np.gcd.reduce(bus_ids)
+def lcm_frequencies(iter_buses: list) -> int:
+    return np.lcm.reduce([b.frequency for b in iter_buses])
 
 
 def find_earliest_bus() -> tuple:
@@ -21,22 +20,32 @@ def find_earliest_bus() -> tuple:
         time += 1
 
 
+def _find_consecutive(index: int, t: int, increment: int, iter_buses: list) -> int:
+    if index <= len(iter_buses) - 2:
+        while True:
+            arrivals = [bus.arrives(t + i) for i, bus in iter_buses[:index + 2]]
+
+            if all(arrivals):
+                new_increment = lcm_frequencies([b[1] for b in iter_buses[:index + 2]])
+                return _find_consecutive(index + 1, t, new_increment, iter_buses)
+
+            t += increment
+
+    return t
+
+
 def find_consecutive_bus_arrival_time() -> int:
     _, buses = read_bus_input()
     eligible_buses = [(i, b) for i, b in enumerate(buses) if not b.out_of_service]
 
-    increment = eligible_buses[0][1].frequency
-    t = ceil(max([b[1].frequency for b in eligible_buses]) / increment) * increment
+    starting_t = eligible_buses[0][1].frequency
 
-    while True:
-        if all([b.arrives(t + i) for i, b in eligible_buses]):
-            return t
-        t += increment
+    time = _find_consecutive(0, starting_t, starting_t, eligible_buses)
+    return time
 
 
 if __name__ == '__main__':
-    # earliest_bus, waiting_minutes = find_earliest_bus()
-    #
-    # print(earliest_bus.frequency * waiting_minutes)
+    earliest_bus, waiting_minutes = find_earliest_bus()
 
+    print(earliest_bus.frequency * waiting_minutes)
     print(f'{find_consecutive_bus_arrival_time()}')
