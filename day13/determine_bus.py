98487425685ab23571839b49c9f925c703cf346f
adelekap <adelejkap@gmail.com>
2020-12-19 23:43:17 -0700

    non performant

--- day13/determine_bus.py
+++ day13/determine_bus.py
@@ -1,4 +1,11 @@
+from math import ceil
+import numpy as np
+
 from day13.input import read_bus_input
+
+
+def heuristic_start(bus_ids: list) -> int:
+    return np.gcd.reduce(bus_ids)
 
 
 def find_earliest_bus() -> tuple:
@@ -14,7 +21,22 @@ def find_earliest_bus() -> tuple:
         time += 1
 
 
-if __name__ == '__main__':
-    earliest_bus, waiting_minutes = find_earliest_bus()
+def find_consecutive_bus_arrival_time() -> int:
+    _, buses = read_bus_input()
+    eligible_buses = [(i, b) for i, b in enumerate(buses) if not b.out_of_service]
 
-    print(earliest_bus.frequency * waiting_minutes)
+    increment = eligible_buses[0][1].frequency
+    t = ceil(max([b[1].frequency for b in eligible_buses]) / increment) * increment
+
+    while True:
+        if all([b.arrives(t + i) for i, b in eligible_buses]):
+            return t
+        t += increment
+
+
+if __name__ == '__main__':
+    # earliest_bus, waiting_minutes = find_earliest_bus()
+    #
+    # print(earliest_bus.frequency * waiting_minutes)
+
+    print(f'{find_consecutive_bus_arrival_time()}')
